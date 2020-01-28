6b10df879b11256daaa3b4ff58ac33c6c15e059b
bamboleylo <tsybulko.artem@gmail.com>
2020-01-28 17:59:48 +0300

    Update structure

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import NavBar from "./Components/Navbar";
+import Profile from "./Components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img src="https://upload.wikimedia.org/wikipedia/ru/c/c3/Aucas_logo.png" alt=""/>
-            </header>
-            <nav className="nav">
-                <div>
-                    <a href="">Profile</a>
-                </div>
-                <div>
-                    <a href="">Messages</a>
-                </div>
-                <div>
-                    <a href="">News</a>
-                </div>
-                <div>
-                    <a href="">Music</a>
-                </div>
-                <div>
-                    <a href="">Settings</a>
-                </div>
-            </nav>
-            <div className="content">
-                <div>
-                    <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" alt=""/>
-                </div>
-                <div>
-                    ava+description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>
-                            post 1
-                        </div>
-                        <div>
-                            post 2
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <NavBar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/ru/c/c3/Aucas_logo.png" alt=""/>
+        </header>
+    );
+}
+
+export default Header;
