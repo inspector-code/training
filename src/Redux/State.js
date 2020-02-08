1c0ca962faca84152f0e8e8584b47b00cfedd9c4
inspector-code <tsybulko.artem@gmail.com>
2020-02-08 12:49:50 +0300

    Update

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -43,30 +43,59 @@ let store = {
         this._callSubscriber = observer;
     },
     dispatch(action){ //  { type: 'ADD-POST' }
-        if (action.type === 'ADD-POST') {
-            let newPost = {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            };
-            this._state.profilePage.posts.push(newPost);
-            this._state.profilePage.newPostText = '';
-            this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state);
-        } else if (action.type === 'ADD-MESS') {
-            let newMessage = {
-                id: 5,
-                message: this._state.dialogsPage.newMessageText
-            };
-            this._state.dialogsPage.messages.push(newMessage);
-            this._state.dialogsPage.newMessageText = '';
-            this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
-            this._state.dialogsPage.newMessageText = action.newMessage;
-            this._callSubscriber(this._state);
+        switch (action.type) {
+            case 'ADD-POST':
+                let newPost = {
+                    id: 5,
+                    message: this._state.profilePage.newPostText,
+                    likesCount: 0
+                };
+                this._state.profilePage.posts.push(newPost);
+                this._state.profilePage.newPostText = '';
+                this._callSubscriber(this._state);
+                break;
+            case 'UPDATE-NEW-POST-TEXT':
+                this._state.profilePage.newPostText = action.newText;
+                this._callSubscriber(this._state);
+                break;
+            case 'ADD-MESS':
+                let newMessage = {
+                    id: 5,
+                    message: this._state.dialogsPage.newMessageText
+                };
+                this._state.dialogsPage.messages.push(newMessage);
+                this._state.dialogsPage.newMessageText = '';
+                this._callSubscriber(this._state);
+                break;
+            case 'UPDATE-NEW-MESSAGE-TEXT':
+                this._state.dialogsPage.newMessageText = action.newMessage;
+                this._callSubscriber(this._state);
+                break;
         }
+        // if (action.type === 'ADD-POST') {
+        //     let newPost = {
+        //         id: 5,
+        //         message: this._state.profilePage.newPostText,
+        //         likesCount: 0
+        //     };
+        //     this._state.profilePage.posts.push(newPost);
+        //     this._state.profilePage.newPostText = '';
+        //     this._callSubscriber(this._state);
+        // } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        //     this._state.profilePage.newPostText = action.newText;
+        //     this._callSubscriber(this._state);
+        // } else if (action.type === 'ADD-MESS') {
+        //     let newMessage = {
+        //         id: 5,
+        //         message: this._state.dialogsPage.newMessageText
+        //     };
+        //     this._state.dialogsPage.messages.push(newMessage);
+        //     this._state.dialogsPage.newMessageText = '';
+        //     this._callSubscriber(this._state);
+        // } else if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
+        //     this._state.dialogsPage.newMessageText = action.newMessage;
+        //     this._callSubscriber(this._state);
+        // }
     }
 };
 
