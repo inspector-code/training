fca56975c2433f6448ee324fee87c34095ce2522
inspector-code <tsybulko.artem@gmail.com>
2020-02-29 13:47:12 +0300

    Update

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,6 +5,7 @@ import {required} from "../../Utils/Validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../Redux/auth-reducer";
 import {Redirect} from "react-router-dom";
+import style from "./../Common/FormsControls/FormsControls.module.css";
 
 const LoginForm = (props) => {
     return (
@@ -30,6 +31,9 @@ const LoginForm = (props) => {
                            component={Input}
                     /> remember me
                 </div>
+                {props.error && <div className={style.formSummaryError}>
+                    {props.error}
+                </div>}
                 <div>
                     <button>Login</button>
                 </div>
--- src/Components/Profile/ProfileContainer.js
+++ src/Components/Profile/ProfileContainer.js
@@ -9,16 +9,16 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 6031;
+            userId = this.props.authorizedUserId;
         }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
     }
 
     render() {
-
         return (
-                <Profile {...this.props} profile={this.props.profile}
+                <Profile {...this.props}
+                         profile={this.props.profile}
                          status={this.props.status}
                          updateStatus={this.props.updateStatus}
                 />
@@ -29,6 +29,8 @@ class ProfileContainer extends React.Component {
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
     status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth,
 });
 
 export default compose(
