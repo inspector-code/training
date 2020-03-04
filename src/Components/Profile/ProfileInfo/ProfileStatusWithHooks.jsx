d55ebe386be9c6e728243720cf3319559456347b
bamboleylo <tsybulko.artem@gmail.com>
2020-03-04 17:48:52 +0300

    Update

--- src/Components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/Components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,22 +1,26 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const ProfileStatusWithHooks = (props) => {
 
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
 
+    useEffect(() => {
+        setStatus(props.status);
+    });
+
     const activateEditMode = () => {
         setEditMode(true);
-    }
+    };
 
     const deactivateEditMode = () => {
         setEditMode(false);
         props.updateStatus(status);
-    }
+    };
 
     const onStatusChange = (e) => {
         setStatus(e.currentTarget.value);
-    }
+    };
 
     return (
         <div>
@@ -32,6 +36,6 @@ const ProfileStatusWithHooks = (props) => {
             }
         </div>
     )
-}
+};
 
 export default ProfileStatusWithHooks;
