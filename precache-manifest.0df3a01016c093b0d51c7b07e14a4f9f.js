bc3d27048fee68e25eb24057d921e280d5d1f5b2
inspector-code <tsybulko.artem@gmail.com>
2020-03-14 14:58:58 +0300

    Updates

---
+++ precache-manifest.0df3a01016c093b0d51c7b07e14a4f9f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefd8d1aaf923dc93c6ecb777213bb3d",
+    "url": "/training/index.html"
+  },
+  {
+    "revision": "cbf12dc2d289b1a2f623",
+    "url": "/training/static/css/3.1a6b97ef.chunk.css"
+  },
+  {
+    "revision": "aa71c694d6887aa61a2e",
+    "url": "/training/static/css/4.92f45987.chunk.css"
+  },
+  {
+    "revision": "4c50cb5618fd1f28ca05",
+    "url": "/training/static/css/main.ec8c302f.chunk.css"
+  },
+  {
+    "revision": "7878ad8802f8f5d8ee3b",
+    "url": "/training/static/js/2.2a1d6cde.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/training/static/js/2.2a1d6cde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbf12dc2d289b1a2f623",
+    "url": "/training/static/js/3.d1e465d1.chunk.js"
+  },
+  {
+    "revision": "aa71c694d6887aa61a2e",
+    "url": "/training/static/js/4.262f2967.chunk.js"
+  },
+  {
+    "revision": "4c50cb5618fd1f28ca05",
+    "url": "/training/static/js/main.81e4ee7e.chunk.js"
+  },
+  {
+    "revision": "68d615475ac1368411cf",
+    "url": "/training/static/js/runtime-main.478a896b.js"
+  },
+  {
+    "revision": "2e9b306272e604a54f1c14c72b9ddbe0",
+    "url": "/training/static/media/Bean Eater-1s-200px.2e9b3062.svg"
+  },
+  {
+    "revision": "7cc7a630624d20f7797cb4c8e93c09c1",
+    "url": "/training/static/media/user.7cc7a630.png"
+  }
+]);
